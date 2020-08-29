1b855dbc9110d041fc2fc4a738d3fe9c024d69ab
Leanne Chen <lianchennnn@gmail.com>
2020-08-29 16:04:34 +0800

    Use mockData to generate styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import imgBomb from './img/bomb-solid.svg';
 import './App.css';
 
 function App() {
@@ -8,66 +7,93 @@ function App() {
       x: 1,
       y: 1,
       hasMine: false,
-      showNumber: 1,
-      isOpened: true,
+      showNumber: 0,
+      isOpened: false,
     },
     {
       x: 2,
       y: 1,
       hasMine: false,
-      showNumber: 1,
+      showNumber: 0,
       isOpened: true,
     },
     {
       x: 3,
       y: 1,
+      hasMine: false,
+      showNumber: 0,
+      isOpened: false,
+    },
+    {
+      x: 1,
+      y: 2,
+      hasMine: false,
+      showNumber: 0,
+      isOpened: false,
+    },
+    {
+      x: 2,
+      y: 2,
+      hasMine: false,
+      showNumber: 2,
+      isOpened: true,
+    },
+    {
+      x: 3,
+      y: 2,
       hasMine: false,
       showNumber: 1,
       isOpened: true,
     },
     {
       x: 1,
-      y: 2,
-      hasMine: false,
-      showNumber: 1,
-      isOpened: true,
-    },
-    {
-      x: 2,
-      y: 2,
-      hasMine: false,
-      showNumber: 1,
-      isOpened: true,
-    },
-    {
-      x: 3,
-      y: 2,
-      hasMine: false,
-      showNumber: 1,
-      isOpened: true,
-    },
-    {
-      x: 1,
       y: 3,
-      hasMine: false,
-      showNumber: 1,
+      hasMine: true,
+      showNumber: 0,
       isOpened: true,
     },
     {
       x: 2,
       y: 3,
       hasMine: false,
-      showNumber: 1,
-      isOpened: true,
+      showNumber: 0,
+      isOpened: false,
     },
     {
       x: 3,
       y: 3,
       hasMine: false,
-      showNumber: 1,
-      isOpened: true,
+      showNumber: 0,
+      isOpened: false,
     },
   ]
+
+  const getSquareClassNames = ({ isOpened, showNumber, hasMine }) => {
+    let classes = ["square"];
+    if(!isOpened) {
+      return classes;
+    }
+    if(hasMine) {
+      return [
+        ...classes,
+        "opened",
+        "bomb",
+      ].join(" ");
+    } else if(showNumber > 0) {
+      return [
+        ...classes,
+        "opened",
+        `number-${showNumber}`,
+      ].join(" ");
+    } else {
+      return [
+        ...classes,
+        "opened",
+      ].join(" ");
+    }
+
+  }
+
   return (
     <div className="App">
       <main className="main">
@@ -76,10 +102,12 @@ function App() {
           { mockData.map((item) =>
             <li 
               key={`${item.x}${item.y}`} 
-              className={`square ${item.x === 2 && item.y === 2 && "opened bomb"} number-2`}
+              className={getSquareClassNames(item)}
+              // className={["square", "opened"].join(" ")}
             >
-              {/*{ item.x === 2 && item.y === 2 && "2" }*/}
+              { item.showNumber > 0 && item.showNumber }
               {/*X{item.x}Y{item.y}*/}
+
             </li>)
           }
         </ul>
