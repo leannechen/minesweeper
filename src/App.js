7773b1165a408369d8f72aa5dee887a17d21a4cd
Leanne Chen <lianchennnn@gmail.com>
2020-08-29 16:54:42 +0800

    Turn App to class component

--- src/App.js
+++ src/App.js
@@ -1,75 +1,80 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  
-  const mockData = [
-    {
-      x: 1,
-      y: 1,
-      hasMine: false,
-      showNumber: 0,
-      isOpened: false,
-    },
-    {
-      x: 2,
-      y: 1,
-      hasMine: false,
-      showNumber: 0,
-      isOpened: true,
-    },
-    {
-      x: 3,
-      y: 1,
-      hasMine: false,
-      showNumber: 0,
-      isOpened: false,
-    },
-    {
-      x: 1,
-      y: 2,
-      hasMine: false,
-      showNumber: 0,
-      isOpened: false,
-    },
-    {
-      x: 2,
-      y: 2,
-      hasMine: false,
-      showNumber: 2,
-      isOpened: true,
-    },
-    {
-      x: 3,
-      y: 2,
-      hasMine: false,
-      showNumber: 1,
-      isOpened: true,
-    },
-    {
-      x: 1,
-      y: 3,
-      hasMine: true,
-      showNumber: 0,
-      isOpened: true,
-    },
-    {
-      x: 2,
-      y: 3,
-      hasMine: false,
-      showNumber: 0,
-      isOpened: false,
-    },
-    {
-      x: 3,
-      y: 3,
-      hasMine: false,
-      showNumber: 0,
-      isOpened: false,
-    },
-  ]
+const mockData = [
+  {
+    x: 1,
+    y: 1,
+    hasMine: false,
+    showNumber: 0,
+    isOpened: false,
+  },
+  {
+    x: 2,
+    y: 1,
+    hasMine: false,
+    showNumber: 0,
+    isOpened: true,
+  },
+  {
+    x: 3,
+    y: 1,
+    hasMine: false,
+    showNumber: 0,
+    isOpened: false,
+  },
+  {
+    x: 1,
+    y: 2,
+    hasMine: false,
+    showNumber: 0,
+    isOpened: false,
+  },
+  {
+    x: 2,
+    y: 2,
+    hasMine: false,
+    showNumber: 2,
+    isOpened: true,
+  },
+  {
+    x: 3,
+    y: 2,
+    hasMine: false,
+    showNumber: 1,
+    isOpened: true,
+  },
+  {
+    x: 1,
+    y: 3,
+    hasMine: true,
+    showNumber: 0,
+    isOpened: true,
+  },
+  {
+    x: 2,
+    y: 3,
+    hasMine: false,
+    showNumber: 0,
+    isOpened: false,
+  },
+  {
+    x: 3,
+    y: 3,
+    hasMine: false,
+    showNumber: 0,
+    isOpened: false,
+  },
+]
 
-  const getSquareClassNames = ({ isOpened, showNumber, hasMine }) => {
+
+class App extends React.Component {
+
+  state = {
+    squareList: [],
+  }
+
+  getSquareClassNames = ({ isOpened, showNumber, hasMine }) => {
     let classes = ["square"];
     if(isOpened) {
       classes = [ ...classes, "opened"];
@@ -83,25 +88,27 @@ function App() {
     return classes.join(" ");
   }
 
-  return (
-    <div className="App">
-      <main className="main">
-        <h1 className="site-title">Minesweeper</h1>
-        <ul className="board">
-          { mockData.map((item) =>
-            <li 
-              key={`${item.x}${item.y}`} 
-              className={getSquareClassNames(item)}
-            >
-              { item.showNumber > 0 && item.showNumber }
-              {/*X{item.x}Y{item.y}*/}
+  render() {
+    return (
+      <div className="App">
+        <main className="main">
+          <h1 className="site-title">Minesweeper</h1>
+          <ul className="board">
+            { mockData.map((item) =>
+              <li
+                key={`${item.x}${item.y}`}
+                className={this.getSquareClassNames(item)}
+              >
+                { item.showNumber > 0 && item.showNumber }
+                {/*X{item.x}Y{item.y}*/}
 
-            </li>)
-          }
-        </ul>
-      </main>
-    </div>
-  );
+              </li>)
+            }
+          </ul>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
