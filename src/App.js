6e5bacd18ff7165a8a86f76652964655165ab852
Leanne Chen <lianchennnn@gmail.com>
2020-08-29 15:18:15 +0800

    Create board markup, styling squares

--- src/App.js
+++ src/App.js
@@ -2,8 +2,86 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const mockData = [
+    {
+      x: 1,
+      y: 1,
+      hasMine: false,
+      showNumber: 1,
+      isOpened: true,
+    },
+    {
+      x: 2,
+      y: 1,
+      hasMine: false,
+      showNumber: 1,
+      isOpened: true,
+    },
+    {
+      x: 3,
+      y: 1,
+      hasMine: false,
+      showNumber: 1,
+      isOpened: true,
+    },
+    {
+      x: 1,
+      y: 2,
+      hasMine: false,
+      showNumber: 1,
+      isOpened: true,
+    },
+    {
+      x: 2,
+      y: 2,
+      hasMine: false,
+      showNumber: 1,
+      isOpened: true,
+    },
+    {
+      x: 3,
+      y: 2,
+      hasMine: false,
+      showNumber: 1,
+      isOpened: true,
+    },
+    {
+      x: 1,
+      y: 3,
+      hasMine: false,
+      showNumber: 1,
+      isOpened: true,
+    },
+    {
+      x: 2,
+      y: 3,
+      hasMine: false,
+      showNumber: 1,
+      isOpened: true,
+    },
+    {
+      x: 3,
+      y: 3,
+      hasMine: false,
+      showNumber: 1,
+      isOpened: true,
+    },
+  ]
   return (
     <div className="App">
+      <main className="main">
+        <h1>Minesweeper</h1>
+        <ul className="board">
+          { mockData.map((item) =>
+            <li 
+              key={`${item.x}${item.y}`} 
+              className={`square ${item.x === 2 && "opened"}`}
+            >
+              {/*X{item.x}Y{item.y}*/}
+            </li>)
+          }
+        </ul>
+      </main>
     </div>
   );
 }
